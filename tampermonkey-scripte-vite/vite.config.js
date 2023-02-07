781e43d4b1abf8ea61805649c790f23f126d7394
Ziyuan Han <hanziyuan08@outlook.com>
2023-02-07 15:59:37 +0800

    refactor: 引入vitejs

---
+++ tampermonkey-scripte-vite/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import monkey, { cdn } from 'vite-plugin-monkey';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    monkey({
+      entry: 'src/main.js',
+      userscript: {
+        name: 'ZenTao',
+        namespace: 'https://iin.ink',
+        version: 2.5,
+        include: /^https:\/\/zentao.*$/,
+        grant: ['GM_addStyle', 'GM_setClipboard'],
+      },
+      build: {
+        externalGlobals: {
+          vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
+        },
+      },
+    }),
+  ],
+});
