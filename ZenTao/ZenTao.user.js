56ac6f013bf2429e633d0e6626594e1117826d93
gucovip <gucovip@gmail.com>
2023-02-09 16:00:45 +0800

    fix: 修复看板样式问题

--- ZenTao/ZenTao.user.js
+++ ZenTao/ZenTao.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ZenTao
 // @namespace    https://iin.ink
-// @version      2.6
+// @version      2.7
 // @description  ZenTao style and function enhancement
 // @author       happy share org
 // @include      /^https:\/\/zentao.*$/
@@ -382,6 +382,7 @@
   const executionIframe = document.querySelector('#appIframe-execution')
   if (executionIframe) {
     executionIframe.onload = function () {
+      setTimeout(() => executionIframe.contentWindow.dispatchEvent(new Event('resize')), 500)
       const doc = executionIframe.contentWindow.document
       enhanceTask(doc)
       enhanceKanBan(doc)
