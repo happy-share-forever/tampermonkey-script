801235b3693666bdd80510241d661a7a386e39c3
gucovip <gucovip@gmail.com>
2023-02-09 15:19:28 +0800

    fix: 修复样式问题

--- ZenTao/ZenTao.user.js
+++ ZenTao/ZenTao.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ZenTao
 // @namespace    https://iin.ink
-// @version      2.5
+// @version      2.6
 // @description  ZenTao style and function enhancement
 // @author       happy share org
 // @include      /^https:\/\/zentao.*$/
@@ -11,18 +11,19 @@
 
 (function () {
   'use strict'
-  GM_addStyle('td.c-name, td.c-title {white-space: normal;}')
-  GM_addStyle('.main-table tbody>tr:nth-child(odd) { background: #fff !important; }')
-  GM_addStyle('.main-table tbody>tr.table-children { background: #90939442 !important; }')
-  GM_addStyle('.main-table .c-actions { width: 300px !important; text-align: left; }')
-  GM_addStyle('.datatable-head-span.datatable-span.fixed-right,.datatable-rows-span.datatable-span.fixed-right { width: 300px !important; text-align: left; }')
-  GM_addStyle('.main-table .c-actions-5 { width: 300px !important; text-align: left; }')
-  GM_addStyle('.chosen-container .chosen-drop { right: 0; }')
+  // 宽屏适配
   GM_addStyle('#main .container { max-width: unset !important; }')
-  GM_addStyle('.table-datatable { min-width: unset !important; }')
-  GM_addStyle('.board-item > .title { max-height: unset !important; -webkit-line-clamp: unset !important; -webkit-box-orient: unset !important; font-size: 15px !important; }')
-  GM_addStyle('#kanban .group-title { line-height: 20px !important; font-size: 15px !important; }')
-  GM_addStyle('.histories-custom-filter-btn { margin-right: 8px }')
+  GM_addStyle('.m-execution-task td.c-name, td.c-title {white-space: normal;}')
+  GM_addStyle('.m-execution-task .main-table tbody>tr:nth-child(odd) { background: #fff !important; }')
+  GM_addStyle('.m-execution-task .main-table tbody>tr.table-children { background: #90939442 !important; }')
+  GM_addStyle('.m-execution-task .main-table .c-actions { width: 300px !important; text-align: left; }')
+  GM_addStyle('.m-execution-task .datatable-head-span.datatable-span.fixed-right,.datatable-rows-span.datatable-span.fixed-right { width: 300px !important; text-align: left; }')
+  GM_addStyle('.m-execution-task .main-table .c-actions-5 { width: 300px !important; text-align: left; }')
+  GM_addStyle('.m-execution-task .chosen-container .chosen-drop { right: 0; }')
+  GM_addStyle('.m-execution-task .table-datatable { min-width: unset !important; }')
+  GM_addStyle('.m-execution-task .board-item > .title { max-height: unset !important; -webkit-line-clamp: unset !important; -webkit-box-orient: unset !important; font-size: 15px !important; }')
+  GM_addStyle('.m-execution-task #kanban .group-title { line-height: 20px !important; font-size: 15px !important; }')
+  GM_addStyle('.m-execution-task .histories-custom-filter-btn { margin-right: 8px }')
   // 根据需要修改分支前缀
 
   const _window = window
